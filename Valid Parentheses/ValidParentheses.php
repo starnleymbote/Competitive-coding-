8d2aa98b41e574cb7dde041b8e7015ae7e1d656e
starno <starnleymbote@gmail.com>
2022-12-06 18:15:28 +0300

    Added valid parenthesis

---
+++ Valid Parentheses/ValidParentheses.php
@@ -0,0 +1,18 @@
+<?php
+
+class SmallestPositiveMissingInteger
+{
+/**
+ * -------------------------------------------------------------|
+ * SAMPLE INPUTS AND EXPECTED OUTCOME                           |
+ * -------------------------------------------------------------|
+ * 
+ * $input = [9, -5, 1, 5, 3] expected results : 2
+ * $input = [1, 2, 3] expected results : 4
+ * $input = [-1, -3] expected result : 1
+ * 
+ */
+
+
+ 
+}
